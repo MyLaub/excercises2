596fddcb59765480159026b9793888b88cba8507
My <idla0003@stud.kea.dk>
2025-09-22 10:25:24 +0200

    update

---
+++ todo_app/js.js
@@ -0,0 +1,5 @@
+const input = document.querySelector(".todoinput");
+const button = document.querySelector("button");
+const todoList = document.querySelector("#todolist");
+
+button.addEventListener("click", addTask);
